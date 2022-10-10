92b2d6f8334b8c62700006b9a9ade5f3a2e3c8fb
Megumi Ando <remiria@flame-daybreak.net>
2022-10-10 21:45:55 +0900

    マルチ言語対応をやってみる

--- scripts/utils.py
+++ scripts/utils.py
@@ -22,3 +22,50 @@ def getFolderReplaceString():
                 buff.update(i)
             return buff
     return {}
+
+
+def getHeaderYaml(path):
+    """
+    mdのヘッダーに書かれているYaml形式のメタデータをDictで取得する
+    """
+
+    with codecs.open(path, "r", 'utf-8') as f:
+        lines = f.readlines()
+
+    flg = False
+
+    retVal = []
+
+    for i in lines:
+        if flg and i.strip() == "---":
+            # 最後の ---
+            break
+        elif not flg and i.strip() == "---":
+            # 最初の ---
+            flg = True
+        else:
+            retVal.append(i)
+
+    if len(retVal):
+        return yaml.safe_load("".join(retVal))
+    return None
+
+
+def getMainDocs(path):
+
+    with codecs.open(path, "r", 'utf-8') as f:
+        lines = f.readlines()
+
+    flg = 0
+
+    docs = []
+
+    for i in lines:
+        i = i.strip()
+        if i == "---":
+            flg += 1
+        else:
+            if flg == 2:
+                docs.append(i)
+
+    return docs
