7e99bcdcfa8a591fe39a865d61ab37664f4a7714
Megumi Ando <remiria@flame-daybreak.net>
2022-12-25 22:27:50 +0900

    Notionの記事ページを生成できるようにしてみる

---
+++ scripts/create_notion_page.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
+import codecs
+
+from notion2md.exporter.block import MarkdownExporter
+from notion_client import Client
+
+
+def createNotionPages():
+
+    md_root = f"{os.getcwd()}/docs/70_Memo"
+
+    # ファイルは作り直し
+    if os.path.exists(md_root):
+        shutil.rmtree(md_root)
+    os.makedirs(md_root)
+
+    notion = Client(auth=os.environ['NOTION_TOKEN'])
+
+    # markdown化したいページを取得
+    db = notion.databases.query(
+        **{
+            'database_id': os.environ['NOTION_DATABASE_ID']
+        }
+    )
+
+    for i in db['results']:
+        title = i['properties']['Name']['title'][0]['plain_text']
+        tags = [x['name'] for x in i['properties']['Tags']['multi_select']]
+        subdir = i['properties']['dir']['rich_text']
+
+        if not i['properties']['publish']['checkbox']:
+            # publishがFalseのものはファイルを作らない
+            continue
+
+        header = ["---"]
+        header.append(f'title: {title}')
+        if len(tags):
+            header.append('tags:')
+        for tag in tags:
+            header.append(f"    - {tag}")
+        header.append('---')
+
+        outputPath = md_root
+
+        if len(subdir):
+            outputPath += "/" + subdir[0]['plain_text']
+
+        MarkdownExporter(block_id=i['id'], output_path=outputPath, download=True, unzipped=True).export()
+
+        markdownName = f"{outputPath}/{i['id']}.md"
+
+        with codecs.open(markdownName, 'r', 'utf-8') as f:
+            lines = [x + "\n" for x in header] + f.readlines()
+
+        with codecs.open(markdownName, 'w', 'utf-8') as f:
+            f.write("".join(lines))
+
+        fname = i['properties']['filename']['rich_text']
+        if len(fname):
+            os.rename(markdownName, f"{outputPath}/{fname[0]['plain_text']}.md")
+
+
+if __name__ == '__main__':
+
+    createNotionPages()
