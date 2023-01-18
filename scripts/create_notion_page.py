dccbdcf1a27093c9e0d2f4de61b2c737b5718b14
Megumi Ando <remiria@flame-daybreak.net>
2023-01-18 12:18:14 +0900

    編集

--- scripts/create_notion_page.py
+++ scripts/create_notion_page.py
@@ -4,9 +4,81 @@ import os
 import shutil
 import codecs
 import sys
+import copy
 
 from notion2md.exporter.block import MarkdownExporter
 from notion_client import Client
+
+
+def getParentItem(parentId):
+
+    notion = Client(auth=os.environ['NOTION_TOKEN'])
+    return notion.pages.retrieve(parentId)
+
+
+def createGroupPages(data, rootDir):
+
+    dirName = data['properties']['dir']['rich_text']
+
+    if len(dirName) == 0:
+        # ルートの場合はなにもしない
+        return
+
+    title = data['properties']['Name']['title'][0]['plain_text']
+
+    path = f"{rootDir}/{dirName[0]['text']['content']}/.pages"
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    with open(path, 'w') as f:
+        f.write(f'title: {title}')
+
+
+def createPage(data, outputPath):
+    # 引数の dataをもとにMarkdownのページを作る
+
+    title = data['properties']['Name']['title'][0]['plain_text']
+    tags = [x['name'] for x in data['properties']['Tags']['multi_select']]
+    subdir = data['properties']['dir']['rich_text']
+    description = data['properties']['description']['rich_text']
+
+    # Markdownのヘッダに追加する文字列を作る
+    header = ["---"]
+    header.append(f'title: {title}')
+    header.append('tags:')
+    header.append(f"    - NotionMemo")
+    for tag in tags:
+        header.append(f"    - {tag}")
+    if len(description):
+        header.append(f"description: {description[0]['plain_text']}")
+    header.append('---')
+
+    savePath = copy.deepcopy(outputPath)
+
+    p = data['properties']['parentItem']['relation']
+
+    if len(p) > 0:
+        parentData = getParentItem(p[0]['id'])
+        dirName = parentData['properties']['dir']['rich_text']
+        if len(dirName) > 0:
+            savePath = savePath + "/" + dirName[0]['text']['content']
+
+    print(savePath)
+
+    if len(subdir) > 0:
+        savePath = savePath + "/" + subdir[0]['text']['content']
+
+    MarkdownExporter(block_id=data['id'], output_path=savePath, download=True, unzipped=True).export()
+
+    markdownName = f"{savePath}/{data['id']}.md"
+
+    with codecs.open(markdownName, 'r', 'utf-8') as f:
+        lines = [x + "\n" for x in header] + f.readlines()
+
+    with codecs.open(markdownName, 'w', 'utf-8') as f:
+        f.write("".join(lines))
+
+    fname = data['properties']['filename']['rich_text']
+    if len(fname):
+        os.rename(markdownName, f"{savePath}/{fname[0]['plain_text']}.md")
 
 
 def createNotionPages():
@@ -28,46 +100,21 @@ def createNotionPages():
     )
 
     for i in db['results']:
-        title = i['properties']['Name']['title'][0]['plain_text']
-        tags = [x['name'] for x in i['properties']['Tags']['multi_select']]
-        subdir = i['properties']['dir']['rich_text']
-        description = i['properties']['description']['rich_text']
+
+        if len(i['properties']['subItem']['relation']):
+            # 子を持つGroupの場合、サブフォルダと .pages を作る
+            createGroupPages(i, md_root)
 
         if not i['properties']['publish']['checkbox']:
             # publishがFalseのものはファイルを作らない
             continue
 
-        header = ["---"]
-        header.append(f'title: {title}')
-        header.append('tags:')
-        header.append(f"    - NotionMemo")
-        for tag in tags:
-            header.append(f"    - {tag}")
-        if len(description):
-            header.append(f"description: {description[0]['plain_text']}")
-        header.append('---')
-
-        outputPath = md_root
-
-        if len(subdir):
-            outputPath += "/" + subdir[0]['plain_text']
-
-        MarkdownExporter(block_id=i['id'], output_path=outputPath, download=True, unzipped=True).export()
-
-        markdownName = f"{outputPath}/{i['id']}.md"
-
-        with codecs.open(markdownName, 'r', 'utf-8') as f:
-            lines = [x + "\n" for x in header] + f.readlines()
-
-        with codecs.open(markdownName, 'w', 'utf-8') as f:
-            f.write("".join(lines))
-
-        fname = i['properties']['filename']['rich_text']
-        if len(fname):
-            os.rename(markdownName, f"{outputPath}/{fname[0]['plain_text']}.md")
+        createPage(i, md_root)
 
 
 if __name__ == '__main__':
+
+    debug = True
 
     os.environ['NOTION_TOKEN'] = sys.argv[1]
     os.environ['NOTION_DATABASE_ID'] = sys.argv[2]
