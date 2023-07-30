72b15451aae2c891b634ac2b2dac7ca9e0da211e
Megumi Ando <remiria@flame-daybreak.net>
2023-07-30 22:05:25 +0900

    ファイル名をテストから変える

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,6 +1,6 @@
 import React from "react";
 import MDXComponents from "@theme-original/MDXComponents";
-import ExpandLink from "@site/src/components/sample.js";
+import ExpandLink from "@site/src/components/expand_link.js";
 import CodePen from "@site/src/components/codepen.js";
 import { TwitterTweetEmbed } from "react-twitter-embed";
 import Gist from "react-gist";
