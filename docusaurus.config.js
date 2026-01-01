32778a3f160ef56056fea0257864ad00612362bd
Megumi Ando <remiria@flame-daybreak.net>
2026-01-02 00:42:17 +0900

    docusaurusバージョンアップ

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,11 @@ const config = {
 	baseUrl: "/reincarnation_tech/",
 
 	onBrokenLinks: "throw",
-	onBrokenMarkdownLinks: "warn",
+	markdown: {
+		hooks: {
+			onBrokenMarkdownLinks: "warn",
+		},
+	},
 
 	// Even if you don't use internalization, you can use this field to set useful
 	// metadata like html lang. For example, if your site is Chinese, you may want
